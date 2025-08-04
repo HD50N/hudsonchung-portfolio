15a6d6a2ad9a5a3aabe90e9e7beefb7af5f34486
Hudson Chung <hudsonch10@gmail.com>
2025-08-03 23:36:17 -0700

    updated phc from phx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { PostHogProvider } from 'posthog-js/react'
 import './index.css'
 import App from './App.jsx'
 
+
+//USE PHC NOT PHX
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2025-05-24',
